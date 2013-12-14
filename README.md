e2614d6b307de9cb4f018aac9dd905943818424a
Alistair Duggin <alistair.duggin@btinternet.com>
2013-12-14 11:10:36 +0000

    adding rails project and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 frontend-exemplar
 =================
+
+The idea here is to show how to develop front end code which is maintainable, universally usable (accessible), and performant.
+
+
+Topics and pages I'm looking to build include:
+
+/
+/styleguide
+/components
+/components/all
+/components/breadcrumb
+/layouts
+/forms
+/forms-feedback
+
+/page-template
+/webfonts
+/icons
+
+/code-standards/
+/code-standards/html
+/code-standards/css
+/code-standards/sass
+/code-standards/javascript
+
+/performance
+/accessibility
+
+
+Tools I'll be looking at include:
+
+sass
+singularity
+wraith
+jasmine
---
+++ exemplar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exemplar::Application.config.secret_key_base = '896e21cb68bcf51e2b84ae961dd1ee474560618e625a7c1decdd260ca72e7a851006ce131c9c1be95f0407122fa485c8cf2f78d1387f53e61afc0dd3504af47a'
